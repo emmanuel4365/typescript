39b62d786e13a9315192a1139d8a18db1da15a76
Omale Arumona <omalearumona@gmail.com>
2024-05-23 09:51:42 +0100

    started

---
+++ type1/build/js/main.js
@@ -0,0 +1,9 @@
+"use strict";
+let userName = "Omale";
+console.log(userName);
+let a = 14;
+let b = 2;
+let c = "3";
+let re = /\w+/g;
+console.log(a / b);
+console.log(c + a);
---
+++ type1/src/main.ts
@@ -0,0 +1,10 @@
+let userName = "Omale";
+console.log(userName);
+
+let a: number = 14;
+let b: number = 2;
+let c: string = "3";
+let re: RegExp = /\w+/g;
+
+console.log(a / b);
+console.log(c + a);
